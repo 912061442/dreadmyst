47f1b5a04ff0584ffbef58b7fa123d9901bdc4d1
912061442 <47880466+912061442@users.noreply.github.com>
2026-02-03 21:29:33 +0800

    Add files via upload

---
+++ Login.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "RenderObjectHolder.h"
+
+class Sprite;
+
+class Login : public RenderObjectHolder
+{
+	public:
+		enum Interface
+		{
+			LoginButton = 1,
+			PhonyButtonUser,
+			PhonyButtonPass,
+			UserPrompt,
+			PassPrompt,
+			RememberTickbox
+		};
+
+	public:
+		Login(RenderObjectHolder& owner, const int id);
+		virtual ~Login();
+
+	private:
+		void input() final;
+		void render() final;
+
+		shared_ptr<Sprite> m_background;
+		shared_ptr<Sprite> m_backgroundGui;
+
+		void* m_chosenPrompt{ nullptr };
+		clock_t m_loginAttemptAt = 0;
+};
+
